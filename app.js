aebdebdf8f3f1f3933897679f5bf5a173e3d98f6
sagatzt <sagatzt@gmail.com>
2024-04-09 10:28:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 80
+const cors = require('cors')
+
+let panels=0
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({response:`panels: ${panels}`})
+})
+
+app.get('/add', (req, res) => {
+  panels++;
+  res.json({response:`(add) panels: ${panels}`})
+})
+
+app.get('/reset', (req, res) => {
+  panels=0
+  res.json({response:`(reset) panels: ${panels}`})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
