1eeaaeed777330b75e6ef23b80fe9d97e3861350
sagatzt <sagatzt@gmail.com>
2024-04-10 13:39:39 +0200

    add socket.io

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express()
 const port = 80
 const cors = require('cors')
@@ -8,12 +9,26 @@ let panels=0
 app.use(cors())
 app.use(express.static('public'))
 
+
+//SOCKETS
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.emit('messages', {panels:panels})  
+})
+
+
+//API
 app.get('/panels', (req, res) => {
   res.json({response:`panels: ${panels}`})
 })
 
 app.get('/add', (req, res) => {
   panels++;
+  io.sockets.emit("messages", {panels:panels})
   res.json({response:`(add) panels: ${panels}`})
 })
 
@@ -22,7 +37,7 @@ app.get('/reset', (req, res) => {
   res.json({response:`(reset) panels: ${panels}`})
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
