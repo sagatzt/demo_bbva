35ff5adfe6a8b6ba1cf3f1edf74e78814094aeb3
sagatzt <sagatzt@gmail.com>
2024-04-12 17:48:47 +0200

    aaaa

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 80
 const cors = require('cors')
 
+let usersConnected=0
 let totalWatts=0
 const maxPower=7400
 
@@ -20,8 +21,13 @@ const io = require("socket.io")(server)
 
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  socket.emit('messages', {totalWatts:totalWatts})  
+  usersConnected++
+  let randomName=getRandomName()
+  console.log(`a new user connected (${randomName}). Total users: ${usersConnected}`)
+  socket.emit('connection', {
+    totalWatts:totalWatts,
+    usersConnected: usersConnected,
+  })  
 
 })
 
@@ -32,12 +38,14 @@ app.get('/charge', (req, res) => {
 })
 
 app.post('/add', (req, res) => {
-  const response=req.body
+  let response=req.body
   if(totalWatts<maxPower) totalWatts+=parseInt(response.watts)
   totalWatts=parseFloat(totalWatts.toFixed(2))
   response.totalWatts=totalWatts
   response.maxPower=maxPower
   response.date=formatDate(new Date())
+  if(response['randomName']==undefined) response['randomName']=getRandomName()
+
   io.sockets.emit("messages", response)
   res.json(response)
 })
@@ -54,22 +62,33 @@ server.listen(port, () => {
 
 
 setInterval(()=>{
-  if(totalWatts>=100) totalWatts-=100
-  io.sockets.emit('messages', {
-    totalWatts:parseFloat(totalWatts.toFixed(2))
+  if(totalWatts>=100) totalWatts-=10
+  io.sockets.emit('stats', {
+    totalWatts:parseFloat(totalWatts.toFixed(2)),
+    usersConnected: usersConnected,
+
   }) 
-},2000)
+},100)
 
 function formatDate(date) {
   let hours = date.getHours()
   let minutes = date.getMinutes()
-  //var ampm = hours >= 12 ? 'pm' : 'am';
+  let seconds = date.getSeconds()
   hours = hours % 12
-  hours = hours ? hours : 12 // the hour '0' should be '12'
+  hours = hours ? hours : 12 
   minutes = minutes < 10 ? '0' + minutes : minutes
-  var strTime = hours + ':' + minutes
+  seconds = seconds < 10 ? '0' + seconds : seconds
+  var strTime = hours + ':' + minutes + ':' + seconds
   return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + strTime
 }
 
+function getRandomName(){
+	var firstnames = [["Jabalí","Toro","Perro","Boquerón","Tiburón","Loro","Gato","Canguro","Ratón","Serpiente","Elefante","Conejo","Burro","Tigre","Jilguero","Avestruz","Lagarto"],["Vaca","Tortuga","Sardina","Oruga","Rata","Cacatua","Gata","Hormiga","Ballena","Mariposa","Trucha","Cobaya","Mula","Avestruz","Lagartija"]]
+	var lastnames= [["Miedoso","Aventurero","Apaleado","Ansiosa","Retorcido","Contento","Adormilado","Feliz","Furioso","Resabiado","Rápido","Resfriado","Loco","Cojo","Lento","Tragón"],["Miedosa","Aventurera","Apaleada","Ansiosa","Retorcida","Contenta","Adormilada","Feliz","Furiosa","Resabiada","Rápida","Resfriada","Loca","Coja","Lenta","Tragona"]]
+  let i=Math.floor(Math.random()*10) % 2
+  var rand_first = Math.floor(Math.random()*firstnames[i].length)
+	var rand_last = Math.floor(Math.random()*lastnames[i].length)
+  return `${firstnames[i][rand_first]} ${lastnames[i][rand_last]}`
+}
 
 
